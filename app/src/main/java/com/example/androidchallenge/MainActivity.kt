521c186e12bac458636aa458cca328c3fb7ad46e
Carolina Alves dos Santos <carolina_alves.s@hotmail.com>
2021-02-28 01:44:10 -0300

    Layout 1.1 Kotlin logic started

--- app/src/main/java/com/example/androidchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androidchallenge/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.androidchallenge
 
-import android.content.Intent
+
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +12,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportActionBar?.hide()
+
+        val text_humidity: TextView = findViewById(R.id.text_humidity)
+        val text_wind_speed: TextView = findViewById(R.id.text_wind_speed)
+        val text_preciptation: TextView = findViewById(R.id.text_preciptation)
+        val text_weather_status: TextView = findViewById(R.id.text_weather_status)
+        val text_max_temperature: TextView = findViewById(R.id.text_max_temperature)
+        val text_min_temperature: TextView = findViewById(R.id.text_min_temperature)
+        val text_current_temperature: TextView = findViewById(R.id.text_current_temperature)
+        var icon_weather_status: ImageView = findViewById(R.id.img_weather_type)
+
+        text_humidity.text = "60%"
+        text_wind_speed.text = "5 km/h"
+        text_preciptation.text = "0 mm"
+        text_weather_status.text = "Partly Cloudy"
+        text_max_temperature.text = "34º"
+        text_min_temperature.text = "18º"
+        text_current_temperature.text = "25"
+        icon_weather_status.setImageResource(R.drawable.ic_image_partlycloudy)
+
+
     }
 
 
